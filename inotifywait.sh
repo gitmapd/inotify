2cd17f4084bfe2fbde702dfb656ca2a80b919ba4
gitmapd <50059062+gitmapd@users.noreply.github.com>
2023-01-06 23:33:46 +0000

    Update inotifywait.sh

--- inotifywait.sh
+++ inotifywait.sh
@@ -1,22 +1,13 @@
 !/usr/bin/env bash
 
-watch="."
-#recursive="--recursive"
+watch=(/root/Cani/ /root/somewhere1/)
+recursive="--recursive"
 exclude=""
-src="/root/Cani/"
-dst="/root/Cani2/"
-sym_changes() {
-my_link="$1"
-echo "mylink: ${my_link}"
-if [[ -L ${my_link} ]] ; then
-      return 0
-   else
-      return 1
-fi
-}
+src=(/root/Cani/ /root/somewhere1/)
+dst=(/root/Cani2/ /root/somewhere2/)
 
 sym_create() {
- ln -s "${1}" "${dst}"  
+ln -s "${1}" "${2}" 
  return
 }
 sym_remove() {
@@ -24,24 +15,53 @@ unlink "${1}"
 return
 }
 
-inotifywait --exclude "${exclude:-\$^}" ${recursive} --monitor "${watch}" |
+inotifywait --exclude "${exclude:-\$^}" "${recursive}" --monitor "${watch[@]}" |
     while read -r directory event file; do
         case "${event}" in
 	    CREATE*)
-		if [[ $? -eq 0 ]] && [[ -d "${file}" ]] ; then
-			sym_create ${src}${file}
+		if [[ $? -eq 0 ]] && [[ "${directory}" == "${watch[0]}" ]]; then
+			   echo "Linking ""${watch[0]}${file}"" to" "${dst[0]}${file}"
+			    sym_create "${src[0]}${file}" "${dst[0]}${file}"
 		else
-			echo "${file} isn't a directory" > /dev/stderr
+			 echo "Linking ""${watch[1]}${file}"" to" "${dst[1]}${file}"
+			sym_create "${src[1]}${file}" "${dst[1]}${file}"
 		fi
-		echo "${file} created" > /dev/stderr
 	    ;;
+    MOVED_FROM*)
+	     if [[ $? -eq 0 ]] && [[ "${directory}" == "${watch[0]}" ]]; then
+		     if [[ -L ${dst[0]}${file} ]]; then
+			     rm -rf "${dst[0]}${file}"
+		     fi
+	     elif [[ $? -eq 0 ]] && [[ "${directory}" == "${watch[1]}" ]]; then
+                     if [[ -L ${dst[1]}${file} ]]; then
+                             rm -rf "${dst[1]}${file}"  
+                     fi
+		else 
+			echo "No changes" > stderr
+		     
+	     fi
+		;;
+	    MOVED_TO*)  
+		    if [[ $? -eq 0 ]] && [[ "${directory}" == "${watch[0]}" ]]; then
+				echo "Linking ""${watch[0]}${file}"" to" "${dst[0]}${file}"
+				sym_create "${src[0]}${file}" "${dst[0]}${file}"
+		   elif [[ $? -eq 0 ]] && [[ "${directory}" == "${watch[1]}" ]]; then
+                                echo "Linking ""${watch[1]}${file}"" to" "${dst[1]}${file}"
+                                sym_create "${src[1]}${file}" "${dst[1]}${file}"
+		   else
+			  echo "No changes" > stderr
+		   fi
+		;;
+
 	    DELETE*)
-		if [[ $? -eq 0 ]] || [[ -d "${file}" ]] ; then
-                        sym_remove ${dst}${file}
+		if [[ $? -eq 0 ]] && [[ "${directory}" == "${watch[0]}" ]]; then
+                           echo "Removing ${dst[0]}${file}"      
+                            sym_remove "${dst[0]}${file}"
                 else
-                        echo "${file} isn't a directory" > /dev/stderr
+                         echo "Removing ${dst[1]}${file}"
+                        sym_remove "${dst[1]}${file}"
                 fi
-		echo "${file} was deleted" > /dev/stderr
-	    ;;
+            ;;
+
         esac
     done
